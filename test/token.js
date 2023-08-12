8b8d929f2c533f721ef8deed1e138cda9158be66
rohitverse <rohitkr23072001@gmail.com>
2023-08-12 11:20:09 +0530

    Initial Commit

--- test/token.js
+++ test/token.js
@@ -10,6 +10,7 @@ describe('ERC20 Token Contract', async () => {
     const Token = await ethers.getContractFactory('Token');
     token = await Token.deploy();
   });
+
   it('Check Token Name', async () => {
     let name = await token.name();
     console.log('Name of the Token ', name);
@@ -33,5 +34,49 @@ describe('ERC20 Token Contract', async () => {
     expect(await token.balanceOf(signer1.address)).to.be.equal(950);
     console.log('Balance of Address 1', await token.balanceOf(signer1.address));
     console.log('Balance of Address 2', await token.balanceOf(signer2.address));
+  });
+
+  describe('Transfer Between Accounts ', async () => {
+    it('Transfer Token to Address 2 to Address 1', async () => {
+      await token.mint();
+      console.log(
+        'Balance of Address 1',
+        await token.balanceOf(signer1.address)
+      );
+      console.log(
+        'Balance of Address 2 ',
+        await token.balanceOf(signer2.address)
+      );
+      await token.transfer(signer2.address, 50);
+      expect(await token.balanceOf(signer1.address)).to.be.equal(950);
+      await token.connect(signer1).transfer(signer2.address, 50);
+      console.log(
+        'Balance of Address 1 ',
+        await token.balanceOf(signer1.address)
+      );
+      console.log(
+        'Balance of Address 2 ',
+        await token.balanceOf(signer2.address)
+      );
+      expect(await token.balanceOf(signer2.address)).to.be.equal(100);
+      console.log(
+        'Balance of Address 1 ',
+        await token.balanceOf(signer1.address)
+      );
+      console.log(
+        'Balance of Address 2 ',
+        await token.balanceOf(signer2.address)
+      );
+    });
+  });
+  describe('Transfer From Function', async () => {
+    it('Check TransferFrom Function ', async () => {
+      await token.mint();
+      // await token.approve( signer1.address , signer2.address , 500);
+      // await token.transferFrom( signer2.address , signer1.address , 50);
+      // console.log(await token.transferFrom( signer2.address , signer1.address , 50));
+      // expect(await token.balanceOf( signer2.address )).to.be.equal(50);
+      // console.log(' Token ' , token );
+    });
   });
 });
